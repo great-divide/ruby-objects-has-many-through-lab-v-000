92e57b699ebd19950e80e1a5dfa9733cb63bacee
Brian Birdwell <bbirdwell@gmail.com>
2018-10-07 04:07:37 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -5,6 +5,7 @@ class Doctor
   def initialize(name)
     @name = name
     @@all << self
+    # @appointments = []
   end
   
   def new_appointment(patient, date)
@@ -12,15 +13,18 @@ class Doctor
   end
   
   def appointments
+    Appointments.all.select { |appt| appt.doctor == self }
+  end
+  
+  def patients
+    Appointments.all.collect 
+  end
     
   def self.all
     @@all
   end
 end
 
-
-# - The `Doctor` class needs an instance method, `#appointments`, that iterates
-#   through all appointments and finds those belonging to this doctor.
 # - The `Doctor` class needs an instance method, `#patients`, that iterates over
 #   that doctor's appointments and collects the patient that belongs to each
 #   appointment.
